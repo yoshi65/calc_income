7ef7e5bccc70c0e111266d82ec8e9d8df0dc1a5d
Yamazaki Yoshitaka <yamazaki@fun.bio.keio.ac.jp>
2018-06-06 12:39:00 +0900

    updated choosing month

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 #
 # FileName: 	calc_income
 # CreatedDate:  2018-06-02 13:44:24 +0900
-# LastModified: 2018-06-04 12:02:47 +0900
+# LastModified: 2018-06-06 12:35:25 +0900
 #
 
 
@@ -11,6 +11,8 @@ import os
 import sys
 import numpy as np
 import pandas as pd
+import argparse
+import re
 from apiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
@@ -21,10 +23,19 @@ from working import Working
 
 
 def main():
+    # option
+    parser = argparse.ArgumentParser(
+        description='calculation income from google calendar')
+    parser.add_argument('-m', '--month', metavar='YYYY-MM', action='store',
+                        nargs='?', help='choice month', default=datetime.now().strftime("%Y-%m"))
+    args = parser.parse_args()
+
     # variable
     data_path = os.path.join(os.path.abspath(
         os.path.dirname(__file__)), 'jobs.csv')
     keyword = pd.read_csv(data_path, header=None)
+    year = int(re.sub(r"-.*$", "", args.month))
+    month = int(re.sub(r"^.*-", "", args.month))
 
     # Setup the Calendar API
     store = file.Storage('credentials.json')
@@ -32,8 +43,8 @@ def main():
     service = build('calendar', 'v3', http=creds.authorize(Http()))
 
     # Call the Calendar API
-    Min = datetime(2018, 5, 11).isoformat() + 'Z'
-    Max = datetime(2018, 6, 10).isoformat() + 'Z'
+    Min = datetime(year, month, 11).isoformat() + 'Z'
+    Max = datetime(year, (month + 1)%12, 10).isoformat() + 'Z'
 
     events_result = service.events().list(calendarId='primary', timeMin=Min,
                                           timeMax=Max, singleEvents=True, orderBy='startTime').execute()
